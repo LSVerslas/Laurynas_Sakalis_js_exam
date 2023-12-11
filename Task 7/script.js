f2ef8a7808b5554b55f7b1ed1cdf9ac02a9e7434
Sakalis <lsverslas@gmail.com>
2023-12-11 11:31:18 +0200

    Audi

--- Task 7/script.js
+++ Task 7/script.js
@@ -1,12 +1,23 @@
-/* ------------------------------ TASK 7 -----------------------------------
-Turimas "audi" objektas.
+'use strict';
+console.log('script.js file was loaded');
 
-Parašykite funkciją "showObjectKeys", kuri kaip argumentą priims objektą 
-ir grąžins visus jo "key" masyve.
--------------------------------------------------------------------------- */
 const audi = {
   make: 'audi',
   model: 'A6',
   year: 2005,
   color: 'white',
 };
+
+function showObjectKeys(obj) {
+  return Object.keys(obj);
+}
+
+function getObjectValues(obj) {
+  return Object.values(obj);
+}
+
+const keysArray = showObjectKeys(audi);
+const valuesArray = getObjectValues(audi);
+
+// console.log(keysArray);
+// console.log(valuesArray);
